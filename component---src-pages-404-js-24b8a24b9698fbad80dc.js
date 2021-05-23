42811c5a31b01cac9136e75140e27ec9284e3694
sschwindt <sschwindt@ucdavis.edu>
2021-05-23 12:22:07 +0200

    Updates

---
+++ component---src-pages-404-js-24b8a24b9698fbad80dc.js
@@ -0,0 +1,2 @@
+(self.webpackChunkgatsby_starter_dimension_v2=self.webpackChunkgatsby_starter_dimension_v2||[]).push([[883],{9616:function(e,t,n){"use strict";n.r(t);var s=n(7294),a=n(4252);t.default=function(){return s.createElement(a.Z,null,s.createElement("h1",null,"NOT FOUND"),s.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-24b8a24b9698fbad80dc.js.map
