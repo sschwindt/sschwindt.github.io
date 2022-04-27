55ac4afe863b657909683b27faaf4245040fedc2
sschwindt <sschwindt@ucdavis.edu>
2022-04-27 19:49:50 +0200

    april update

--- src/components/Main.js
+++ src/components/Main.js
@@ -168,7 +168,7 @@ class Main extends React.Component {
               </a>
             </li>
             <li>
-              <a href="https://scholar.google.ch/citations?user=GdmFKPgAAAAJ&hl=de&oi=ao" target="_blank" rel="noreferrer" className="icon fa-google">
+              <a href="hhttps://scholar.google.com/citations?user=246FKewAAAAJ&hl=en&citsig=AMD79oouG1nzUOL85fNl5zk00cmjothoLQ" target="_blank" rel="noreferrer" className="icon fa-google">
                 <span className="label">Google Scholar</span>
               </a>
             </li>
